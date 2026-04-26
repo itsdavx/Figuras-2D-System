7d49478316575dcd1870682480cb11ec57dab9b5
itsdav_x <davidbonilla188@gmail.com>
2026-04-26 00:14:13 -0500

    Implementacion Funcional: Star

--- Figuras 2D/Figuras 2D/FrmStar.cs
+++ Figuras 2D/Figuras 2D/FrmStar.cs
@@ -1,22 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Figuras_2D.Shapes;
 
 namespace Figuras_2D
 {
     public partial class FrmStar : Form
     {
         private static FrmStar instancia;
-        public FrmStar()
+
+        private Star star;
+
+        private FrmStar()
         {
             InitializeComponent();
+            btnGraficar.Click += btnGraficar_Click;
+            PanelGrafico.Paint += PanelGrafico_Paint;
         }
+
         public static FrmStar Instancia
         {
             get
@@ -27,6 +28,60 @@ namespace Figuras_2D
                 }
                 return instancia;
             }
+        }
+
+        private void btnGraficar_Click(object sender, EventArgs e)
+        {
+            if (!Validar(out float tamanoCm))
+                return;
+
+            int tamanoPx = (int)(tamanoCm * 58f);
+
+            PanelGrafico.Width = (tamanoPx * 2) + 20;
+            PanelGrafico.Height = (tamanoPx * 2) + 20;
+
+            int x = 10;
+            int y = 10;
+
+            star = new Star( x, y, tamanoPx, new Pen(Color.Black, 2), new SolidBrush(Color.Cyan));
+
+            PanelGrafico.Invalidate();
+        }
+
+        private void PanelGrafico_Paint(object sender, PaintEventArgs e)
+        {
+            if (star != null)
+            {
+                star.Draw(e.Graphics);
+            }
+        }
+
+        private bool Validar(out float tamano)
+        {
+            tamano = 0;
+
+            string input = txtTamano.Text.Trim();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                MessageBox.Show("El campo no puede estar vacío");
+                return false;
+            }
+
+            if (!float.TryParse(input, System.Globalization.NumberStyles.Float,
+                System.Globalization.CultureInfo.InvariantCulture, out tamano))
+            {
+                MessageBox.Show("Ingrese un número válido (use punto para decimales)");
+                return false;
+            }
+
+            if (tamano <= 0)
+            {
+                MessageBox.Show("El valor debe ser mayor que 0");
+                return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ Figuras 2D/Figuras 2D/Shapes/Star.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+
+namespace Figuras_2D.Shapes
+{
+    public class Star : Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Tamano { get; set; }
+
+        public Star(int x, int y, int tamano, Pen pen, Brush brush)
+            : base(pen, brush)
+        {
+            X = x;
+            Y = y;
+            Tamano = tamano;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Point[] puntos = new Point[10];
+
+            double angulo = -Math.PI / 2;
+            double incremento = Math.PI / 5; // 36 grados
+
+            int radioExterno = Tamano;
+            int radioInterno = Tamano / 2;
+
+            int centroX = X + radioExterno;
+            int centroY = Y + radioExterno;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int radio = (i % 2 == 0) ? radioExterno : radioInterno;
+
+                int px = (int)(centroX + radio * Math.Cos(angulo));
+                int py = (int)(centroY + radio * Math.Sin(angulo));
+
+                puntos[i] = new Point(px, py);
+
+                angulo += incremento;
+            }
+
+            g.FillPolygon(Brush, puntos);
+            g.DrawPolygon(Pen, puntos);
+        }
+    }
+}
