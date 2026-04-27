0065064116d0a5e81030ec34fa4e51d8c1b589b4
Danny <dannytiupul@gmail.com>
2026-04-26 23:34:09 -0500

    Figura kite agregada

--- Figuras 2D/Figuras 2D/FrmKite.Designer.cs
+++ Figuras 2D/Figuras 2D/FrmKite.Designer.cs
@@ -28,22 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnGraficar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDiagonalMayor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDiagonalMenor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panelGrafico = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // btnGraficar
+            // 
+            this.btnGraficar.Location = new System.Drawing.Point(269, 146);
+            this.btnGraficar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGraficar.Name = "btnGraficar";
+            this.btnGraficar.Size = new System.Drawing.Size(84, 29);
+            this.btnGraficar.TabIndex = 42;
+            this.btnGraficar.Text = "Graficar";
+            this.btnGraficar.UseVisualStyleBackColor = true;
+            this.btnGraficar.Click += new System.EventHandler(this.btnGraficar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(233, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 20);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "cm";
+            // 
+            // txtDiagonalMayor
+            // 
+            this.txtDiagonalMayor.Location = new System.Drawing.Point(168, 102);
+            this.txtDiagonalMayor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDiagonalMayor.Name = "txtDiagonalMayor";
+            this.txtDiagonalMayor.Size = new System.Drawing.Size(58, 26);
+            this.txtDiagonalMayor.TabIndex = 40;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 20);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Diagonal Mayor:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 29);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Gráfico:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 29);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Inputs:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 20);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "cm";
+            // 
+            // txtDiagonalMenor
+            // 
+            this.txtDiagonalMenor.Location = new System.Drawing.Point(168, 149);
+            this.txtDiagonalMenor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDiagonalMenor.Name = "txtDiagonalMenor";
+            this.txtDiagonalMenor.Size = new System.Drawing.Size(58, 26);
+            this.txtDiagonalMenor.TabIndex = 44;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 20);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "Diagonal Menor:";
+            // 
+            // panelGrafico
+            // 
+            this.panelGrafico.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelGrafico.Location = new System.Drawing.Point(36, 267);
+            this.panelGrafico.Name = "panelGrafico";
+            this.panelGrafico.Size = new System.Drawing.Size(697, 560);
+            this.panelGrafico.TabIndex = 46;
+            this.panelGrafico.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGrafico_Paint);
             // 
             // FrmKite
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1784, 1061);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(2007, 1326);
+            this.Controls.Add(this.panelGrafico);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDiagonalMenor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnGraficar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDiagonalMayor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmKite";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kite";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGraficar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDiagonalMayor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDiagonalMenor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panelGrafico;
     }
 }
--- Figuras 2D/Figuras 2D/FrmKite.cs
+++ Figuras 2D/Figuras 2D/FrmKite.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,116 @@ namespace Figuras_2D
                 }
                 return instancia;
             }
+        }
+
+
+        float d1Cm, d2Cm;
+        bool dibujar = false;
+
+        private void panelGrafico_Paint(object sender, PaintEventArgs e)
+        {
+            if (!dibujar)
+                return;
+
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            int d1Px = (int)(d1Cm * 58f);
+            int d2Px = (int)(d2Cm * 58f);
+
+            float alpha = 0.3f;
+
+            int arriba = (int)(d1Px * alpha);
+            int abajo = d1Px - arriba;
+            int alturaTotal = arriba + abajo;
+
+            int x = (panelGrafico.Width - d2Px) / 2;
+            int y = (panelGrafico.Height - alturaTotal) / 2;
+
+            Point[] puntos = new Point[]
+            {
+        new Point(x + d2Px / 2, y),
+        new Point(x + d2Px, y + arriba),
+        new Point(x + d2Px / 2, y + alturaTotal),
+        new Point(x, y + arriba)
+            };
+
+            g.FillPolygon(Brushes.MediumPurple, puntos);
+
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                g.DrawPolygon(pen, puntos);
+            }
+
+        }
+
+        private bool ValidarKite(out float d1, out float d2)
+        {
+            d1 = 0;
+            d2 = 0;
+
+            string input1 = txtDiagonalMayor.Text.Trim();
+            string input2 = txtDiagonalMenor.Text.Trim();
+
+            if (string.IsNullOrEmpty(input1) || string.IsNullOrEmpty(input2))
+            {
+                MessageBox.Show("Ambos campos son obligatorios");
+                return false;
+            }
+                       
+            if (!float.TryParse(input1, System.Globalization.NumberStyles.Float,
+                System.Globalization.CultureInfo.InvariantCulture, out d1))
+            {
+                MessageBox.Show("Diagonal mayor inválida (use punto para decimales)");
+                return false;
+            }
+
+            if (!float.TryParse(input2, System.Globalization.NumberStyles.Float,
+                System.Globalization.CultureInfo.InvariantCulture, out d2))
+            {
+                MessageBox.Show("Diagonal menor inválida (use punto para decimales)");
+                return false;
+            }
+
+            if (d1 <= 0 || d2 <= 0)
+            {
+                MessageBox.Show("Los valores deben ser mayores que 0");
+                return false;
+            }
+                        
+            if (d2 >= d1)
+            {
+                MessageBox.Show("La diagonal menor debe ser menor que la mayor");
+                return false;
+            }
+
+            if (d1 > 50 || d2 > 50)
+            {
+                MessageBox.Show("Valores demasiado grandes (máx 50 cm recomendado)");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnGraficar_Click(object sender, EventArgs e)
+        {
+            if (!ValidarKite(out d1Cm, out d2Cm))
+            {
+                dibujar = false;
+                panelGrafico.Invalidate();
+                return;
+            }
+
+            dibujar = true;
+
+            int d1Px = (int)(d1Cm * 58f);
+            int d2Px = (int)(d2Cm * 58f);
+
+            panelGrafico.Width = d2Px + 40;
+            panelGrafico.Height = d1Px + 40;
+
+            panelGrafico.Invalidate();
         }
     }
 }
